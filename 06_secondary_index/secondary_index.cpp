7b084308e3c04d7d9789e9a9f2f2f1c0978ee118
Gleb <liftchampion@gmail.com>
2019-09-05 03:49:29 +0300

    06 WIP

--- 06_secondary_index/secondary_index.cpp
+++ 06_secondary_index/secondary_index.cpp
@@ -4,6 +4,7 @@
 #include <map>
 #include <string>
 #include <unordered_map>
+#include <string_view>
 
 using namespace std;
 
@@ -17,19 +18,93 @@ struct Record {
 
 // Реализуйте этот класс
 class Database {
+private:
+	multimap<int, string_view> time_to_id;
+	multimap<int, string_view> karma_to_id;
+	multimap<string_view, string_view> user_to_id;
+	struct RecordData {
+		explicit RecordData(const Record& rc) : rc(rc) {}
+		Record rc;
+		mutable decltype(time_to_id.begin()) time_it = {};
+		mutable decltype(karma_to_id.begin()) karma_it = {};
+		mutable decltype(user_to_id.begin()) user_it = {};
+	};
+	unordered_map<string_view, RecordData> id_to_data;
 public:
-	bool Put(const Record& record);
-	const Record* GetById(const string& id) const;
-	bool Erase(const string& id);
+	bool Put(const Record& record)
+	{
+		if (id_to_data.find(record.id) == id_to_data.end()) {
+			return false;
+		}
+
+		RecordData rd(record);
+		unordered_map<string_view, RecordData>::node_type new_node;
+		new_node.mapped() = move(rd);
+		new_node.key() = new_node.mapped().rc.id;
+
+		const auto it_and_ret = id_to_data.insert(move(new_node));
+		auto extracted = id_to_data.extract(record.id);
+		const auto& record_data = it_and_ret.position->second;
+		record_data.time_it =
+				time_to_id.insert({record_data.rc.timestamp, record_data.rc.id});
+		record_data.karma_it =
+				karma_to_id.insert({record_data.rc.karma, record_data.rc.id});
+		record_data.user_it =
+				user_to_id.insert({record_data.rc.user, record_data.rc.id});
+		return true;
+	}
+	const Record* GetById(const string& id) const {
+		const auto it = id_to_data.find(id);
+		return it == id_to_data.end() ? nullptr : &it->second.rc;
+
+	}
+	bool Erase(const string& id) {
+		const auto it = id_to_data.find(id);
+		if (it == id_to_data.end()) {
+			return false;
+		} else {
+			const RecordData& rd = it->second;
+			time_to_id.erase(rd.time_it);
+			karma_to_id.erase(rd.karma_it);
+			user_to_id.erase(rd.user_it);
+			id_to_data.erase(it);
+			return true;
+		}
+	}
 
 	template <typename Callback>
-	void RangeByTimestamp(int low, int high, Callback callback) const;
+	void RangeByTimestamp(int low, int high, Callback callback) const
+	{
+		auto begin = time_to_id.lower_bound(low);
+		const auto end = time_to_id.end();
+		while (begin != end && begin->first <= high &&
+			callback(id_to_data.at(begin->second).rc)) {
+			++begin;
+		}
+	}
 
 	template <typename Callback>
-	void RangeByKarma(int low, int high, Callback callback) const;
+	void RangeByKarma(int low, int high, Callback callback) const {
+		auto begin = karma_to_id.lower_bound(low);
+		const auto end = karma_to_id.end();
+		while (begin != end && begin->first <= high &&
+			   callback(id_to_data.at(begin->second).rc)) {
+			++begin;
+		}
+	}
 
 	template <typename Callback>
-	void AllByUser(const string& user, Callback callback) const;
+	void AllByUser(const string& user, Callback callback) const {
+		auto begin = user_to_id.find(user);
+		const auto end = user_to_id.end();
+		while (begin != end && begin->first == user &&
+			   callback(id_to_data.at(begin->second).rc)) {
+			++begin;
+		}
+	}
+
+private:
+
 };
 
 void TestRangeBoundaries() {
@@ -41,8 +116,9 @@ void TestRangeBoundaries() {
 	db.Put({"id2", "O>>-<", "general2", 1536107260, bad_karma});
 
 	int count = 0;
-	db.RangeByKarma(bad_karma, good_karma, [&count](const Record&) {
+	db.RangeByKarma(bad_karma, good_karma, [&count](const Record& rc) {
 		++count;
+		cout << rc.user << endl;
 		return true;
 	});
 
